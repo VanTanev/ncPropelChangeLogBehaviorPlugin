cc4ff3da4687d5c47c364d146d939b0b410c1c37
CraftyShadow <vankata.t@gmail.com>
2010-09-04 19:53:21 +0300

    initial commit

---
+++ modules/ncchangelogentry/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * ncchangelogentry actions.
+ *
+ * @package    sumarios
+ * @subpackage ncchangelogentry
+ * @author     ncuesta
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class ncchangelogentryActions extends autoNcchangelogentryActions
+{
+}
